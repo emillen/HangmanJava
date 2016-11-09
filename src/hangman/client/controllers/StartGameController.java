a4084188087c2286936b86db3c59fcf0404e437c
Emil Lengman <emillen@kth.se>
2016-11-09 20:29:41 +0100

    intializing view

--- src/hangman/client/controllers/StartGameController.java
+++ src/hangman/client/controllers/StartGameController.java
@@ -1,5 +1,8 @@
 package hangman.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 import java.net.Socket;
 
 /**
@@ -9,8 +12,17 @@ public class StartGameController {
 
     private Socket socket;
 
+    @FXML
+    Text scoreText;
 
-    public void init(Socket socket) {
+    public void init(Socket socket, int score) {
         this.socket = socket;
+
+        scoreText.setText("Total Score: " + score);
+    }
+
+    @FXML
+    public void newGame(){
+        System.out.println("I see you want new game bitch?");
     }
 }
